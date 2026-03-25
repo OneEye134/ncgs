32a788f3684284aea56e8079914f5fd6f4aa365a
OneEye134 <christianeduardalarcon@gmail.com>
2026-03-25 22:12:27 +0800

    PLEASE WORK STYLE

--- api/index.py
+++ api/index.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
+@app.route('/OUR.SCHOOL.LIFE')
+def home():
+    return render_template('osl.html')
